29cf85ae8b4fa381e36143d2c21f2b2d010042b8
Yokeso <460063539@qq.com>
2022-03-26 21:29:39 +0800

    原型文件提交

--- regard-Android/app/src/main/java/com/example/regard/MainActivity.java
+++ regard-Android/app/src/main/java/com/example/regard/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.regard;
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.Regard.Message";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMessage(View view){
+    }
+
+
 }
